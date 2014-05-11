bbbec87c0988a50cee10f47a639a96311978ca5c
Mike Lavazza <lavazhole@yahoo.com>
2014-05-11 15:29:23 -0500

    push

---
+++ Plot2.R
@@ -0,0 +1,26 @@
+#Read in dataset
+
+setwd("./ExploratoryDataAnalysis")
+full = read.csv("household_power_consumption.csv", header=TRUE, sep=';')
+full$DateTime = (strptime(paste(full$Date, full$Time), "%d/%m/%Y %H:%M:%OS"))
+full$Date = as.Date(strptime(full$Date, "%d/%m/%Y"))
+full$Time = (strptime(full$Time, "%H:%M:%OS"))
+full$Global_active_power = as.numeric(as.character(full$Global_active_power))
+full$Voltage = as.numeric(as.character(full$Voltage))
+full$Global_reactive_power = as.numeric(as.character(full$Global_reactive_power))
+full$Sub_metering_1 = as.numeric(as.character(full$Sub_metering_1))
+full$Sub_metering_2 = as.numeric(as.character(full$Sub_metering_2))
+full$Sub_metering_3 = as.numeric(as.character(full$Sub_metering_3))
+
+#Create graph 1
+
+data1 = full[as.Date(full$Date) == "2007-02-01",]
+data2 = full[as.Date(full$Date) == "2007-02-02",]
+power = rbind(data1, data2)
+
+rm(data1)
+rm(data2)
+
+plot(power$DateTime, power$Global_active_power, typ='l', xlab = "", ylab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot2.png")
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,36 @@
+#Read in dataset
+
+setwd("./ExploratoryDataAnalysis")
+full = read.csv("household_power_consumption.csv", header=TRUE, sep=';')
+full$DateTime = (strptime(paste(full$Date, full$Time), "%d/%m/%Y %H:%M:%OS"))
+full$Date = as.Date(strptime(full$Date, "%d/%m/%Y"))
+full$Time = (strptime(full$Time, "%H:%M:%OS"))
+full$Global_active_power = as.numeric(as.character(full$Global_active_power))
+full$Voltage = as.numeric(as.character(full$Voltage))
+full$Global_reactive_power = as.numeric(as.character(full$Global_reactive_power))
+full$Sub_metering_1 = as.numeric(as.character(full$Sub_metering_1))
+full$Sub_metering_2 = as.numeric(as.character(full$Sub_metering_2))
+full$Sub_metering_3 = as.numeric(as.character(full$Sub_metering_3))
+
+#Create graph 1
+
+data1 = full[as.Date(full$Date) == "2007-02-01",]
+data2 = full[as.Date(full$Date) == "2007-02-02",]
+power = rbind(data1, data2)
+
+rm(data1)
+rm(data2)
+
+par(mfrow=c(2,2), mar = c(5,5,1,5))
+plot(power$DateTime, power$Global_active_power, typ='l', xlab = "", ylab="Global Active Power (kilowatts)")
+
+plot(power$DateTime, power$Voltage, typ='l', xlab = "datetime", ylab="Voltage")
+
+plot(power$DateTime, power$Sub_metering_1, typ='l', col="black", xlab="", ylab="Energy sub metering")
+lines(power$DateTime, power$Sub_metering_2, col="red")
+lines(power$DateTime, power$Sub_metering_3, col="blue")
+legend('topright', legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), col=c("black", "red", "blue"), inset=.005, bty = 0, lty=1, box.lty=0)
+
+plot(power$DateTime, power$Global_reactive_power, typ='l', xlab = "datetime", ylab="Global_reactive_power")
+dev.copy(png, file="plot4.png")
+dev.off()
